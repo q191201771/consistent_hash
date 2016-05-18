c02e309ac4fa55843f93a4dc6a829af0361b86bc
q191201771 <191201771@qq.com>
2016-05-18 22:28:07 +0800

    first commit

---
+++ consistent_hash.go
@@ -0,0 +1,95 @@
+package consistent_hash
+
+import (
+	"errors"
+	"fmt"
+	"hash/crc32"
+	"sort"
+	"sync"
+)
+
+var errNoNode = errors.New("consistent hash: no node exist")
+
+type uints []uint32
+
+func (arr uints) Len() int           { return len(arr) }
+func (arr uints) Less(i, j int) bool { return arr[i] < arr[j] }
+func (arr uints) Swap(i, j int)      { arr[i], arr[j] = arr[j], arr[i] }
+
+type ConsistentHash struct {
+	sync.RWMutex
+
+	nodes         map[uint32]string
+	points        uints
+	virtualMultis int
+}
+
+func Default() *ConsistentHash {
+	return New(20)
+}
+
+func New(virutalMultis int) *ConsistentHash {
+	return &ConsistentHash{
+		nodes:         make(map[uint32]string),
+		virtualMultis: virutalMultis,
+	}
+}
+
+func (c *ConsistentHash) Add(node string) {
+	c.Lock()
+	defer c.Unlock()
+
+	for i := 0; i < c.virtualMultis; i++ {
+		point := c.hashKey(c.virtualPoint(node, i))
+		c.nodes[point] = node
+	}
+	c.update()
+}
+
+func (c *ConsistentHash) Remove(node string) {
+	c.Lock()
+	defer c.Unlock()
+
+	for i := 0; i < c.virtualMultis; i++ {
+		point := c.hashKey(c.virtualPoint(node, i))
+		delete(c.nodes, point)
+	}
+	c.update()
+}
+
+func (c *ConsistentHash) Get(key string) (string, error) {
+	c.RLock()
+	defer c.RUnlock()
+
+	if len(c.points) == 0 {
+		return "", errNoNode
+	}
+	point := c.hashKey(key)
+	return c.nodes[c.search(point)], nil
+}
+
+func (c *ConsistentHash) update() {
+	c.points = nil
+	for k, _ := range c.nodes {
+		c.points = append(c.points, k)
+	}
+	sort.Sort(c.points)
+}
+
+func (c *ConsistentHash) virtualPoint(node string, index int) string {
+	return fmt.Sprintf("%s%d", node, index)
+}
+
+func (c *ConsistentHash) hashKey(key string) uint32 {
+	return crc32.ChecksumIEEE([]byte(key))
+}
+
+func (c *ConsistentHash) search(point uint32) uint32 {
+	index := sort.Search(len(c.points), func(i int) bool {
+		return c.points[i] >= point
+	})
+	if index < len(c.points) {
+		return c.points[index]
+	}
+	return c.points[0]
+}
---
+++ consistent_hash_test.go
@@ -0,0 +1,129 @@
+package consistent_hash
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestError(t *testing.T) {
+	ch := Default()
+	_, err := ch.Get("aaa")
+	if err != errNoNode {
+		t.Fatalf("err: [%s] expected [%s]", err.Error(), errNoNode.Error())
+	}
+}
+
+func TestBasic(t *testing.T) {
+	ch := Default()
+	ch.Add("node2")
+	ch.Add("node3")
+	ch.Add("node4")
+	ch.Add("node5")
+
+	keys := []string{
+		"apple",
+		"banana",
+		"car",
+		"dog",
+		"eat",
+		"fuck",
+		"give",
+		"hive",
+		"ing",
+		"jet",
+		"ken",
+		"long",
+		"man",
+		"no",
+	}
+	fmt.Println("-----First:")
+	for _, v := range keys {
+		node, _ := ch.Get(v)
+		fmt.Println(v, ":", node)
+	}
+	fmt.Println("-----Second:")
+	for _, v := range keys {
+		node, _ := ch.Get(v)
+		fmt.Println(v, ":", node)
+	}
+	ch.Add("node6")
+	ch.Add("node1")
+	fmt.Println("-----Third")
+	for _, v := range keys {
+		node, _ := ch.Get(v)
+		fmt.Println(v, ":", node)
+	}
+	ch.Remove("node2")
+	ch.Remove("node5")
+	fmt.Println("-----Fourth")
+	for _, v := range keys {
+		node, _ := ch.Get(v)
+		fmt.Println(v, ":", node)
+	}
+}
+
+func TestHuge(t *testing.T) {
+	var keys []string
+	for i := 0; i < 10000; i++ {
+		keys = append(keys, UUID())
+	}
+
+	nodeCount := make(map[string]int)
+	ch := New(1000)
+	ch.Add("ajsk1")
+	ch.Add("12ejio1")
+	ch.Add("2r02")
+	ch.Add("nk")
+	for _, key := range keys {
+		node, _ := ch.Get(key)
+		if _, exist := nodeCount[node]; !exist {
+			nodeCount[node] = 0
+		}
+		nodeCount[node]++
+	}
+	fmt.Println("-----Huge First")
+	for k, v := range nodeCount {
+		fmt.Println(k, ":", v)
+	}
+
+	for k := range nodeCount {
+		delete(nodeCount, k)
+	}
+	ch.Remove("2r02")
+	ch.Remove("ajsk1")
+	for _, key := range keys {
+		node, _ := ch.Get(key)
+		if _, exist := nodeCount[node]; !exist {
+			nodeCount[node] = 0
+		}
+		nodeCount[node]++
+	}
+	fmt.Println("-----Huge Second")
+	for k, v := range nodeCount {
+		fmt.Println(k, ":", v)
+	}
+
+	for k := range nodeCount {
+		delete(nodeCount, k)
+	}
+	ch.Add("2r")
+	ch.Add("a1")
+	for _, key := range keys {
+		node, _ := ch.Get(key)
+		if _, exist := nodeCount[node]; !exist {
+			nodeCount[node] = 0
+		}
+		nodeCount[node]++
+	}
+	fmt.Println("-----Huge Third")
+	for k, v := range nodeCount {
+		fmt.Println(k, ":", v)
+	}
+}
+
+func UUID() string {
+	out, _ := exec.Command("uuidgen").Output()
+	return strings.TrimSuffix(strings.TrimSuffix(string(out), "\n"), "\r\n")
+}
